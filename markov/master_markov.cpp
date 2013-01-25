0edad4fdc82da34beed4aa5eb6d138909d594449
Matias Fernandez <vectorns@zeratul.(none)>
2013-01-25 21:58:39 -0200

    Donde anda LA JERGA

--- markov/master_markov.cpp
+++ markov/master_markov.cpp
@@ -67,61 +67,77 @@ void createMatrix(DIR* dp, string pathbegin) {
   wordcell* tmpwordcell;
   int currentrank;
   
-  //cout << "Master: Creating matrix....\n";
+  //////cerr << "Master: Creating matrix....\n";
   while (dirp = readdir(dp)) {
-    //cout << "Master: Check if is a valid book.\n";
+    //////cerr << "Master: Check if is a valid book.\n";
     if (string(dirp->d_name) == "." || string(dirp->d_name) == "..") continue;
 
     filepath = pathbegin + "/" + string(dirp->d_name);   
-    //cout << "Master: Reading book " << filepath << endl;
+    //////cerr << "Master: Reading book " << filepath << endl;
     ifstream fin(filepath.c_str());
     if (fin.is_open()) {
-      //cout << "Master: The file is ok!" << endl;
+      //////cerr << "Master: The file is ok!" << endl;
     }
     else {
-      //cout << "Master: Bad file" << endl; 
+      //////cerr << "Master: Bad file" << endl; 
     }
     previousword = INITWORD;
     while (!fin.eof()) {
+      isina = false;
+      isinp = false;
       getline(fin, line, ' ');
       line.erase(line.find_last_not_of(" \n\r\t")+1);
-      //cout << "Master: Readed word " << line << endl;
-      for (vector<routecell>::iterator it2 = routetable.begin(); it2 < routetable.end(); it2++) {
-	if ((*it2).word == previousword || previousword == INITWORD) {
-	  isinp = true;
-	  rankp = (*it2).rank;
-	}
-	else if ((*it2).word == line) {
-	  isina = true;
-	  ranka = (*it2).rank;
-	}
-	if (isinp && isina) {
-	  break;
-	}
-      }
+      //////cerr << "Master: Readed word " << line << endl;
       if (previousword == INITWORD) {
-	//cout << "Master: First word of book" << endl; 
+	//////cerr << "Master: First word of book" << endl; 
+	for (vector<routecell>::iterator it2 = routetable.begin(); it2 < routetable.end(); it2++) {
+	  if ((*it2).word == line) {
+	    isina = true;
+	    ranka = (*it2).rank;
+	    break;
+	  }
+	}
 	if (!isina) {
 	  tmproutecell = new routecell();
 	  (*tmproutecell).prob = 1;
 	  (*tmproutecell).word = line;
 	  (*tmproutecell).rank = nextrank;
 	  routetable.push_back(*tmproutecell);
+	  tmpwordcell = new wordcell();
+	  (*tmpwordcell).word = line;
+	  (*tmpwordcell).rank = nextrank;
+	  wordtable.push_back(*tmpwordcell);
 	  nextrank = (nextrank+1) % NUMTASKS;
 	  nextrank = (nextrank == 0 ? 1 : nextrank); 
-	  //cout << "Master: New word " << line << endl;
+	  ////cerr << "Master: New word to master " << (*tmpwordcell).word << " with rank " << (*tmpwordcell).rank << endl;
 	}
 	else {
 	  for (vector<routecell>::iterator it2 = routetable.begin(); it2 < routetable.end(); it2++) {
 	    if ((*it2).word == line) {
 	      (*it2).prob++;
-	      //cout << "Master: Know word " << line << " - Quantity " << (*it2).prob << endl;
+	      //////cerr << "Master: Know word " << line << " - Quantity " << (*it2).prob << endl;
 	      break;
 	    }
 	  }
 	}
       }
       else {
+	////cerr << "Master: Serching previousword " << previousword << " and actualword " << line << endl;
+	for (vector<wordcell>::iterator it2 = wordtable.begin(); it2 < wordtable.end(); it2++) {
+	  if ((*it2).word == previousword) {
+	    isinp = true;
+	    rankp = (*it2).rank;
+	    ////cerr << "Master: Rank p is " << rankp << endl;
+	  }
+	  else if ((*it2).word == line) {
+	    isina = true;
+	    ranka = (*it2).rank;
+	    ////cerr << "Master: Rank a is " << ranka << endl;
+	  }
+	  if (isinp && isina) {
+	    break;
+	  }
+	}
 	if (!isina) {
 	  tmpwordcell = new wordcell();
 	  (*tmpwordcell).word = line;
@@ -130,26 +146,26 @@ void createMatrix(DIR* dp, string pathbegin) {
 	  wordtable.push_back(*tmpwordcell);
 	  nextrank = (nextrank+1) % NUMTASKS;
 	  nextrank = nextrank == 0 ? 1 : nextrank;
+	  ////cerr << "Master: New word to slave " << (*tmpwordcell).word << " with rank " << (*tmpwordcell).rank << endl;
 	}
 	sprintf(numstr, "%d", ranka);
-	//cout << "Master: Sending " << (previousword + SEPARATOR + line + SEPARATOR + numstr + SEPARATOR) << endl;
+	//cerr << "Master: Sending " << (previousword + SEPARATOR + line + SEPARATOR + numstr + SEPARATOR) << " to " << rankp << endl;
 	sendMessage(previousword + SEPARATOR + line + SEPARATOR + numstr + SEPARATOR, rankp);
       }
       previousword = line;	  
-      
     }
     fin.close();
-    //cout << "Master: Sending end of the book " << (previousword + SEPARATOR + ENDWORD + SEPARATOR + "0" + SEPARATOR) << endl;
-    sendMessage(previousword + SEPARATOR + ENDWORD + SEPARATOR + "0" + SEPARATOR, rankp);
+    //cerr << "Master: Sending end of the book " << (previousword + SEPARATOR + ENDWORD + SEPARATOR + "0" + SEPARATOR) << " to " << rankp << endl;
+    sendMessage(previousword + SEPARATOR + ENDWORD + SEPARATOR + "0" + SEPARATOR, ranka);
   }
   closedir(dp);
-  //cout << "Master: Matrix ended.\n";
+  //////cerr << "Master: Matrix ended.\n";
 };
 
 void calculateAndSync() {
-  //cout << "Master: Calculating and sync for " << NUMTASKS << " tasks" << endl;
+  //////cerr << "Master: Calculating and sync for " << NUMTASKS << " tasks" << endl;
   for (int i=1; i < NUMTASKS; i++)  {
-    //cout << "Master: Sending end construct to " << i << endl;
+    //////cerr << "Master: Sending end construct to " << i << endl;
     sendMessage(STOPCONSTRUCT + SEPARATOR, i);
   }
   int totalwords = 0;
@@ -160,7 +176,7 @@ void calculateAndSync() {
     (*it).prob = (*it).prob / totalwords;
   }
   vector<routecell> unsort = routetable;
-  vector<routecell> sorted(unsort.size());
+  vector<routecell> sorted(0);
   bool isIn = false;
   for (vector<routecell>::iterator it1 = unsort.begin(); it1 < unsort.end(); it1++) {
     for (vector<routecell>::iterator it2 = sorted.begin(); it2 < sorted.end(); it2++) {
@@ -176,20 +192,20 @@ void calculateAndSync() {
   }
   routetable = sorted;
   MPI_Barrier(MPI_COMM_WORLD);
-  cout << "Master: Calculated probabilities and sync after barrier" << endl;
+  ////cerr << "Master: Calculated probabilities and sync after barrier" << endl;
 };
 
 void runBooks() {
   float randinit;
   char numstr[21]; //Note: Enough to hold all numbers up to 64-bits  
-  //cout << "Master: Start to create books " << NUMBOOKS << endl;
+  ////cerr << "Master: Start to create books " << NUMBOOKS << endl;
   for(int i = 0; i < NUMBOOKS; i++) {
     randinit = (((double) rand()) / ((double)RAND_MAX));
-    //cout << "Master: Creating book number " << i << endl;
+    ////cerr << "Master: Creating book number " << i << endl;
     for(vector<routecell>::iterator it1 = routetable.begin(); it1 < routetable.end(); it1++) {
-      //cout << "Master: Checking word " << (*it1).word << endl;
       if ((randinit = randinit - (*it1).prob) < 0) {
 	sprintf(numstr, "%d", i);
+	////cerr << "Master: Sending " << (*it1).word << " to " << (*it1).rank << endl;
 	sendMessage((*it1).word + SEPARATOR + numstr + SEPARATOR + "0" + SEPARATOR, (*it1).rank);
 	sendMessage((*it1).word + SEPARATOR + numstr + SEPARATOR + "0" + SEPARATOR, 0);
 	break;
@@ -217,29 +233,31 @@ void proccessBooks() {
   stringstream *sstream1, *sstream2;
   while (endbooks < NUMBOOKS) {
     message = receiveMessage();
+    cerr << "Master: Message recived " << message << endl;
     //TODO: Fork to wait more messages and process.
     pos1 = message.find(SEPARATOR);
     word = message.substr(0, pos1);
-    //cout << "Master: Word recived " << word << endl;
+    ////cerr << "Master: Word recived " << word << endl;
     pos2 = message.find(SEPARATOR, pos1+1);
     booknum = message.substr(pos1+1, pos2-(pos1));
     booknum = booknum.substr(1, booknum.length());
     booknum = booknum.substr(0, booknum.length()-1);
-    //cout << "Master: Num book " << booknum << endl;
+    ////cerr << "Master: Num book " << booknum << endl;
     pos3 = message.find(SEPARATOR, pos2+1);
     secnum = message.substr(pos2+1, pos3-(pos2));
     secnum = secnum.substr(1, secnum.length());
     secnum = secnum.substr(0, secnum.length()-1);
-    //cout << "Master: Sec num " << secnum << endl;
+    ////cerr << "Master: Sec num " << secnum << endl;
     if (word == "END") {
+      //cerr << "Master: End " << endbooks << endl;
       endbooks++;
     }
     else {
-      //cout << "Master: Book message " << booknum << endl;
+      ////cerr << "Master: Book message " << booknum << endl;
       sstream1 = new stringstream(booknum);
       (*sstream1) >> pos1;      
       for (vector<book>::iterator it1 = books.begin(); it1 < books.end(); it1++) {
-	//cout << "Master: Checking with book " << (*it1).number << endl;
+	////cerr << "Master: Checking with book " << (*it1).number << endl;
 	if ((*it1).number == pos1) {
 	  sstream2 = new stringstream(secnum);
 	  (*sstream2) >> pos2;
@@ -251,35 +269,43 @@ void proccessBooks() {
       delete sstream1;
     }
   }
+  //cerr << "Master: Resume slaves" << endl;
   string resume = RESUMESLAVE;
   for (int i1=1; i1 < NUMTASKS; i1++) {
     sendMessage(resume + SEPARATOR, i1);
   }
   //TODO: Write books.
   for (vector<book>::iterator it1 = books.begin(); it1 < books.end(); it1++) {
-    cout << "Master: Book number " << (*it1).number << endl;
+    cerr << "Master: Book number " << (*it1).number << endl;
     for (int i1 = 0; i1 < (*it1).words.size(); i1++) {
-      cout << (*it1).words[i1] << " ";
+      cerr << (*it1).words[i1] << " ";
     }
-    cout << endl;
+    cerr << endl;
   }
 };
 
 void master(int ntasks, const char* pathbooks, int nbooks) {
   DIR *dp;
   NUMTASKS = ntasks;
-  //cout << "Master: Number of tasks created " << ntasks << ", and " << nbooks << " books." << endl;
+  //////cerr << "Master: Number of tasks created " << ntasks << ", and " << nbooks << " books." << endl;
   NUMBOOKS = nbooks;
   dp = opendir(pathbooks);
   if (dp == NULL) {
-    cout << "Error opening " << pathbooks << endl;
+    ////cerr << "Error opening " << pathbooks << endl;
     return;
   }
   string spath(pathbooks);
   srand(time(NULL));
   createMatrix(dp, spath);
   calculateAndSync();
+  cerr << "Master: Start to run books." << endl;
+  //  for (vector<routecell>::iterator it2 = routetable.begin(); it2 < routetable.end(); it2++) {
+  //    ////cerr << "\tprob - " << (*it2).prob << "\tword - " << (*it2).word << "\trank - " << (*it2).rank << endl;
+  //  }
   runBooks();
   proccessBooks();
+  //cerr << "Master: Befor barrier." << endl;
+  MPI_Finalize();
+  //err << "Master: End." << endl;
   return;
 };
--- markov/slave_markov.cpp
+++ markov/slave_markov.cpp
@@ -20,7 +20,7 @@ using namespace std;
 
 struct routecell {
   string word;
-  float prob;
+  double prob;
   int rank;
 };
 
@@ -30,6 +30,7 @@ struct column {
 };
 
 vector<column> columns;
+int rank;
 
 void addWord(string word);
 string createMessage(string word,int bookNum, int seqNum);
@@ -39,7 +40,7 @@ routecell searchNextWord(string word);
 routecell addWordToColumn(string word, int rank);
 void calculateAndSyncSlave();
 
-void slave(int rank) {
+void slave(int grank) {
 	string message;
 	string masterMessage;
 	string slaveMessage;
@@ -48,7 +49,8 @@ void slave(int rank) {
 	int seqNum;
 	bool construct = true;	
 	routecell cell;
-	srand(time(NULL));
+	rank = grank;
+	srand(time(NULL) + rank);
 
 	while (construct) {
 		message = receiveMessage();
@@ -59,30 +61,45 @@ void slave(int rank) {
 			addWord(message);
 		}
 	}
+	//cout << "Slave: Printing columns" << endl;
 	calculateAndSyncSlave();
-	cout << "Slave: Start to create books." << endl;
+	cerr << "Slave" << rank << ": Start to create books." << endl;
+	//////cerr << "Slave" << rank << ": Printing columns of " << rank << endl;
+	//for(vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++){
+	//////cerr << "word" << rank << " - " << (*it1).word << endl;
+	//for (vector<routecell>::iterator it2 = (*it1).nextWords.begin(); it2 < (*it1).nextWords.end(); it2++) {
+	//////cerr << "\tprob - " << (*it2).prob << "\tword - " << (*it2).word << "\trank - " << (*it2).rank << endl;
+	//}
+	//}	
+
 	while(true) {      
 	  message = receiveMessage();
-	  //cout << "Slave: Comparation !!!" << message.substr(0, 14) << "!!!" << endl;
+	  ////////cerr << "Slave: Comparation !!!" << message.substr(0, 14) << "!!!" << endl;
 	  if (message.compare("BALANCE") == 0) {
 	    //TODO work overload
 	  }
 	  else if (message.substr(0, 14).compare("<resume-slave>") == 0) {
-	    //cout << "Slave: End slave." << endl;
+	    ////////cerr << "Slave: End slave." << endl;
 	    break;
 	  }
 	  else {
-	    //cout << "Slave: Recive word " << message << endl;
+	    cerr << "Slave" << rank << ": Recive word " << message << endl;
 	    readBookMessage(message, word, bookNum, seqNum);
+	    cerr << "Slave" << rank << ": Read book message" << endl;
 	    cell = searchNextWord(word);
+	    cerr << "Slave" << rank << ": Search word" << endl;
 	    masterMessage = createMessage(word, bookNum, seqNum);
+	    cerr << "Slave" << rank << ": Create Message with " << cell.word << endl;
 	    slaveMessage  = createMessage(cell.word, bookNum, seqNum + 1);
-	    //cout << "Slave: Master message - " << masterMessage << endl;
-	    //cout << "Slave: Slave message - " << slaveMessage << endl;
+	    cerr << "Slave" << rank << ": Master message - " << masterMessage << " to " << cell.rank << endl;
+	    cerr << "Slave" << rank << ": Slave message - " << slaveMessage << " to " << cell.rank << endl;
 	    sendMessage(masterMessage, 0);
 	    sendMessage(slaveMessage, cell.rank);// 0 rank of slav
 	  }	
 	}
+	//cerr << "Slave" << rank << ": Before barrier." << endl;
+	MPI_Finalize();
+	//cerr << "Slave" << rank << ": End " << endl;
 	return;
 }
 
@@ -95,9 +112,9 @@ void slave(int rank) {
 	    routecell rc;
 
 	    readColumnMessage(word, word1, word2, rank);
-	    //cout << "Slave: Rank " << rank << endl;
-	    //cout << "Slave: Word 1 " << word1 << endl;
-	    //cout << "Slave: Word 2 " << word2 << endl;
+	    //////////cerr << "Slave: Rank " << rank << endl;
+	    //////////cerr << "Slave: Word 1 " << word1 << endl;
+	    //////////cerr << "Slave: Word 2 " << word2 << endl;
 	    for (vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++) {
 		    if(word1.compare((*it1).word)==0) {
 			    notInWord1 = false;
@@ -109,19 +126,20 @@ void slave(int rank) {
 				    }
 			    }		
 			    if(notInWord2) {
-				    rc = addWordToColumn(word2,rank);
-				    ((*it1).nextWords.push_back(rc));
-
+			      //////////cerr << "Slave: Adding word 1 " << word2 << endl;
+			      rc = addWordToColumn(word2,rank);
+			      ((*it1).nextWords.push_back(rc));
 			    }
 			    break;
 		    }
 	    }
 	    if(notInWord1) {
-		    column* c = new column();
-		    (*c).word = word1;
-		    columns.push_back(*c);
-		    rc = addWordToColumn(word2,rank);
-		    columns.back().nextWords.push_back(rc);
+	      //////////cerr << "Slave: Adding word 2 " << word2 << endl;
+	      column* c = new column();
+	      (*c).word = word1;
+	      columns.push_back(*c);
+	      rc = addWordToColumn(word2,rank);
+	      columns.back().nextWords.push_back(rc);
 	    }
 
 	    column col;
@@ -130,14 +148,14 @@ void slave(int rank) {
 			    col = (*it1);
 		    }
 	    }
-	    //cout << "Slave " << rank << " atendend word: " << col.word << endl;
+	    //////////cerr << "Slave " << rank << " atendend word: " << col.word << endl;
 
-	    //cout << "Slave " << rank << " goes to : ";
+	    //////////cerr << "Slave " << rank << " goes to : ";
 
-	    for(vector<routecell>::iterator it3 = col.nextWords.begin(); it3 < col.nextWords.end(); it3++) {
-	        cout<< " word: " << (*it3).word << " - " << "rank: " << (*it3).rank << "\t"; 
-	    }
-	    cout << endl;
+	    //for(vector<routecell>::iterator it3 = col.nextWords.begin(); it3 < col.nextWords.end(); it3++) {
+	    //    ////////cerr<< " word: " << (*it3).word << " - " << "rank: " << (*it3).rank << "\t"; 
+	    //}
+	    //////////cerr << endl;
     }
 
     string createMessage(string word, int bookNum, int seqNum) {
@@ -163,29 +181,29 @@ void slave(int rank) {
       string seqStr;
       string bookStr;
 
-      //cout << "Slave: Read book message " << message << endl;
+      //////////cerr << "Slave: Read book message " << message << endl;
       pos1 = message.find("¬");
       word = message.substr(0, pos1);
-      //cout << "Slave: Read book word " << word << endl;
+      //////////cerr << "Slave: Read book word " << word << endl;
       pos2 = message.find("¬", pos1+1);
       bookStr = message.substr(pos1+1, pos2-(pos1));
-      //cout << "Slave: Read book bookStr " << bookStr << endl;
+      //////////cerr << "Slave: Read book bookStr " << bookStr << endl;
       bookStr = bookStr.substr(1, bookStr.length());
       bookStr = bookStr.substr(0, bookStr.length()-1);
-      //cout << "Slave: Read book bookStr " << bookStr << endl;
+      //////////cerr << "Slave: Read book bookStr " << bookStr << endl;
       pos3 = message.find("¬",pos2+1);
       seqStr = message.substr(pos2+1,pos3-(pos2));
-      //cout << "Slave: Read book seqStr " << seqStr << endl;
+      //////////cerr << "Slave: Read book seqStr " << seqStr << endl;
       seqStr = seqStr.substr(1, seqStr.length());
       seqStr = seqStr.substr(0, seqStr.length()-1);      
-      //cout << "Slave: Read book seqStr " << seqStr << endl;
-      //      cout << "Slave: pos1 - " << pos1 << " pos2 - " << pos2 << endl;
+      //////////cerr << "Slave: Read book seqStr " << seqStr << endl;
+      //      ////////cerr << "Slave: pos1 - " << pos1 << " pos2 - " << pos2 << endl;
     
       stringstream convertSeq(seqStr);
       convertSeq >> seqNum;
       stringstream convertBook(bookStr);
       convertBook >> bookNum;
-      //cout << "Slave: word - " << word << " seq - " << seqNum << " book - " << bookNum << endl;
+      //////////cerr << "Slave: word - " << word << " seq - " << seqNum << " book - " << bookNum << endl;
     }
 
     void readColumnMessage(string message, string &wordRequested, string &wordToGo, int &rank) {
@@ -194,46 +212,54 @@ void slave(int rank) {
 	    size_t pos3;
 	    string rankStr;
 
-	    //cout << endl;	
-	    //cout << "Slave: readColumnMessage message : " << message << endl;
-	    //cout << "Slave: readColumnMessage message length: " << message.length() << endl;
+	    //////////cerr << endl;	
+	    //////////cerr << "Slave: readColumnMessage message : " << message << endl;
+	    //////////cerr << "Slave: readColumnMessage message length: " << message.length() << endl;
 	    pos1 = message.find("¬");
 	    wordRequested = message.substr(0, pos1);
-	    //cout << "Slave: readColumnMessage wr: " << wordRequested << endl;
+	    //////////cerr << "Slave: readColumnMessage wr: " << wordRequested << endl;
 	    pos2 = message.find("¬", pos1 + 1);
 	    wordToGo = message.substr(pos1+1, pos2-(pos1));
-	    //cout << "Slave: readColumnMessage wtg: " << wordToGo << endl;
+	    //////////cerr << "Slave: readColumnMessage wtg: " << wordToGo << endl;
 	    wordToGo = wordToGo.substr(1, wordToGo.length());
 	    wordToGo = wordToGo.substr(0, wordToGo.length()-1);
-	    //cout << "Slave: readColumnMessage wtg: " << wordToGo << endl;
-	    //cout << "Slave: pos1 " << pos1 << " pos2 " << pos2 << endl;
+	    //////////cerr << "Slave: readColumnMessage wtg: " << wordToGo << endl;
+	    //////////cerr << "Slave: pos1 " << pos1 << " pos2 " << pos2 << endl;
 	    pos3 = message.find("¬", pos2 + 1);
 	    rankStr = message.substr(pos2+1,pos3-(pos2));
-	    //cout << "Slave: readColumnMessage rs " << rankStr << endl;
+	    //////////cerr << "Slave: readColumnMessage rs " << rankStr << endl;
 	    rankStr = rankStr.substr(1, rankStr.length());
 	    rankStr = rankStr.substr(0, rankStr.length()-1);
-	    //cout << "Slave: readColumnMessage rs " << rankStr << endl;
+	    //////////cerr << "Slave: readColumnMessage rs " << rankStr << endl;
 	    stringstream convertRank(rankStr);
 	    convertRank >> rank;
     }
 
 routecell randomWord(vector<routecell> nextWords){
   double randinit = (((double) rand()) / ((double)RAND_MAX));
-  cout << "Slave: Random number " << randinit << endl;
+  cerr << "Slave" << rank << ": Size vector " << nextWords.size() << endl;
+  double howmuch = 0;
   for(vector<routecell>::iterator it = nextWords.begin(); it < nextWords.end(); it++) {
-    cout << "Slave: Next rand " << randinit << endl;
-    if ((randinit = (randinit - (*it).prob)) < 0) {
-      cout << "Slave: Next word " << (*it).word << endl;
-      return (*it);
-    };
+    howmuch += (*it).prob;
   }
+  cerr << "Slave" << rank << ": How much " << howmuch << endl;
+  for(vector<routecell>::iterator it = nextWords.begin(); it < nextWords.end(); it++) {
+    cerr << "Slave" << rank << ": Current rand " << randinit << endl;
+    cerr << "Slave" << rank << ": Prob " << (*it).prob << endl;
+    cerr << "Slave" << rank << ": Word " << (*it).word << endl;
+    if ((randinit = (randinit - (*it).prob)) <= 0) {
+      return (*it);
+    }
+  }
+  cerr << "Slave" << rank << ": Se pico tuti!" << endl;
 }
 
 routecell searchNextWord(string word) {
+  //cerr << "Slave" << rank << ": Serching..." << endl;
   for (vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++) {
-    //cout << "Slave: Compare " << word << " " << (*it1).word << endl;
+    //cerr << "Slave" << rank << ": Compare " << word << " " << (*it1).word << endl;
     if(word.compare((*it1).word) == 0) {
-      //cout << "Slave: Finded word " << (*it1).word << endl;
+      //cerr << "Slave" << rank << ": Finded word " << (*it1).word << endl;
       return randomWord((*it1).nextWords);
     }
   }
@@ -248,9 +274,8 @@ routecell addWordToColumn(string word, int rank) {
 }
 
 void calculateAndSyncSlave() {
-	
 	for(vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++){
-		int totalwords = 0;
+		double totalwords = 0;
 		for (vector<routecell>::iterator it2 = (*it1).nextWords.begin(); it2 < (*it1).nextWords.end(); it2++) {
 			totalwords += (*it2).prob;
 		}
@@ -258,7 +283,7 @@ void calculateAndSyncSlave() {
 			(*it2).prob = (*it2).prob / totalwords;
 		}
 		vector<routecell> unsort = (*it1).nextWords;
-		vector<routecell> sorted(unsort.size());
+		vector<routecell> sorted(0);
 		bool isIn = false;
 		for (vector<routecell>::iterator it3 = unsort.begin(); it3 < unsort.end(); it3++) {
 			for (vector<routecell>::iterator it4 = sorted.begin(); it4 < sorted.end(); it4++) {
@@ -274,14 +299,14 @@ void calculateAndSyncSlave() {
 		}
 		(*it1).nextWords = sorted;
 	}
-	cout << "Slave: Printing columns" << endl;
-	for(vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++){
-	  cout << "word - " << (*it1).word << endl;
-	  for (vector<routecell>::iterator it2 = (*it1).nextWords.begin(); it2 < (*it1).nextWords.end(); it2++) {
-	    cout << "\tprob - " << (*it2).prob << "\tword - " << (*it2).word << endl;
-	  }
-	}	
+	//////////cerr << "Slave: Printing columns" << endl;
+	//for(vector<column>::iterator it1 = columns.begin(); it1 < columns.end(); it1++){
+	//////////cerr << "word - " << (*it1).word << endl;
+	//  for (vector<routecell>::iterator it2 = (*it1).nextWords.begin(); it2 < (*it1).nextWords.end(); it2++) {
+	//    ////////cerr << "\tprob - " << (*it2).prob << "\tword - " << (*it2).word << endl;
+	//  }
+	//}	
 	
 	MPI_Barrier(MPI_COMM_WORLD);
-	cout << "Slave: Calculated probabilities and sync after barrier" << endl;
+	////////cerr << "Slave: Calculated probabilities and sync after barrier" << endl;
 }
